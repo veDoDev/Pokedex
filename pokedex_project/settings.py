8047c8a13cad5efdd708a6ec16d80f5c2687b229
veDoDev <vedsuniltripathi@gmail.com>
2025-04-21 18:32:03 +0530

    Restored secret key from env

--- pokedex_project/settings.py
+++ pokedex_project/settings.py
@@ -5,7 +5,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-your-secret-key-here'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
